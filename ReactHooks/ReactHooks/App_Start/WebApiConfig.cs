f1ac35d3c5aadcabe8a11b1a4370256c070d9a81
coskunsahin1981@yahoo.com <coskunsahin1981@yahoo.com>
2020-11-19 00:52:50 +0300

    json formant

--- ReactHooks/ReactHooks/App_Start/WebApiConfig.cs
+++ ReactHooks/ReactHooks/App_Start/WebApiConfig.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using System.Web.Http.Cors;
-
+using Newtonsoft.Json.Serialization;
 namespace ReactHooks
 {
     public static class WebApiConfig
@@ -20,7 +20,12 @@ namespace ReactHooks
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
+
+  
             );
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Serialize;
         }
     }
 }
